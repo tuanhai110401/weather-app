1b8997693d0de800b9deb8ea26151a0150747880
tuanhai110401 <haigongon@gmail.com>
2024-09-12 22:29:45 +0700

    Fix UI

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -149,7 +149,7 @@ function Home() {
       </div>
       <div className="home-wrapper">
         <section className="about-wrapper mb mt">
-          <div data-aos="fade-down-right" className="about-info">
+          <div className="about-info">
             <h4 className="home-title">
               Your #1 source of any weather forecasts and updates.
             </h4>
@@ -161,12 +161,12 @@ function Home() {
               and detailed weather forecasts available on any device.
             </p>
           </div>
-          <div data-aos="fade-left" className="about-image">
-            <img src={imageCelendar} alt="celendar" />
+          <div className="about-image">
+            <img src={imgPost2} alt="celendar" />
           </div>
         </section>
 
-        <section className="forecast mb" data-aos="zoom-in">
+        <section className="forecast mb">
           <h5 className="home-title">World weather forecast</h5>
           <p className="home-desc">Please select a country</p>
           <div className="countries-wrapper">
@@ -186,7 +186,7 @@ function Home() {
             ))}
           </div>
         </section>
-        <section className="new-wrapper mb" data-aos="zoom-out-down">
+        <section className="new-wrapper mb">
           <h4 className="home-title">Weather forecast news</h4>
           <p className="home-desc">
             Get the best weather news from our team of meteorologists and learn
@@ -200,7 +200,7 @@ function Home() {
         </section>
 
         <section className="about-wrapper mb">
-          <div className="about-info" data-aos="fade-right">
+          <div className="about-info">
             <h4 className="home-title">Avoid weather surprises</h4>
             <p className="home-desc">
               WeatherHub provides a road trip planner tool with accurate weather
@@ -217,13 +217,13 @@ function Home() {
               plans at any given time.
             </p>
           </div>
-          <div className="about-image" data-aos="fade-left">
+          <div className="about-image">
             <img src={imageUsa} alt="florida" />
           </div>
         </section>
 
         <section className="about-wrapper wrapper-reverse mb">
-          <div className="about-info" data-aos="fade-right">
+          <div className="about-info">
             <h4 className="home-title">Avoid weather surprises</h4>
             <p className="home-desc">
               WeatherHub partners with national weather services and qualified
@@ -237,12 +237,12 @@ function Home() {
             </p>
             <Button className="about-btn">READ OUR STORY</Button>
           </div>
-          <div className="about-image weather" data-aos="fade-left">
+          <div className="about-image weather">
             <img src={imageThunder} alt="thunder" />
           </div>
         </section>
 
-        <section className="contact-wrapper mb" data-aos="zoom-in">
+        <section className="contact-wrapper mb">
           <div className="wrapper">
             <h5 className="home-title">Get the fresh weather forecast daily</h5>
             <p className="home-desc" style={{ width: "50%" }}>
